9b1274a043665a160dad5465d8108dc58b06f0a3
Nícolas dos Santos Carvalho <nicolas.mnw@gmail.com>
2023-11-30 23:06:38 -0300

    modificado para exrcicio

--- main.js
+++ main.js
@@ -8,34 +8,21 @@
 
 // -----------------------------------------------------------------------------
 
-const listaHTML = document.getElementById("lista");
-let contador = 0;
+// ? SELECIONAR A LISTA
 
 function novoItem() {
-  const item = document.createElement("li");
-  item.id = `item-${contador}`;
-  item.className = "item";
-  item.innerHTML = `
-    <input type="checkbox"/>
-    <input class="descricao" placeholder="descrição"></input>g
-    <i onClick="removerItem(${contador++})" class="ph ph-trash"></i>
-  `
-
-  listaHTML.appendChild(item);
+  // ? CRIAR O NOVO ITEM
+  // ? CONFIGURAR O ITEM
+  // ? ADICIONAR A LISTA O NOVO ITEM
 }
 
 function removerItem(id) {
-  listaHTML.querySelector(`#item-${id}.item`).remove();
+  // ? SELECIONAR O ITEM
+  // ? REMOVER O ITEM
 }
 
 function limparLista() {
-  const todos = listaHTML.querySelectorAll(`.item`);
-
-  for (let i = 0; i < todos.length; i++) {
-    const element = todos[i];
-
-    if (element.querySelector("input").checked) {
-      element.remove();
-    }
-  }
+  // ? SELECIONAR TODOS OS ITENS
+  // ? VERIFICAR UM POR UM SE ESTA SELECIONADO
+  // ? REMOVER O ITEM
 }
